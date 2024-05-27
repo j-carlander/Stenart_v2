874e0110a4a358ed0245cf34977fbf984645cb7e
j-carlander <johannes.carlander@gmail.com>
2024-05-27 13:28:22 +0200

    highlights carousel component

---
+++ src/components/HighlightCarousel/HighLightCarousel.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useRef, useState } from "react";
+import "./HighLightCarousel.css";
+
+export function HighLightCarousel({ highlights }) {
+  const [index, setIndex] = useState(0);
+  const timerId = useRef();
+  const timerMs = 5000;
+
+  useEffect(() => {
+    function nextSlide() {
+      setIndex((index) => {
+        let newIndex = index + 1;
+        if (newIndex >= highlights.length) newIndex = 0;
+        return newIndex;
+      });
+    }
+
+    timerId.current = setInterval(nextSlide, timerMs);
+
+    return () => clearInterval(timerId.current);
+  }, [highlights]);
+
+  return (
+    <article className="highlights-carousel">
+      <ul className="slides">
+        {highlights.map((highlight, imgIndex) => (
+          <li
+            className="slide"
+            key={highlight.id}
+            data-active={imgIndex === index}>
+            <img src={`/images/${highlight.id}.png`} alt="" />
+          </li>
+        ))}
+      </ul>
+    </article>
+  );
+}
