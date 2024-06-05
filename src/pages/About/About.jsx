9a091e9578096c64419623bc06f7568c21f55f3c
j-carlander <johannes.carlander@gmail.com>
2024-06-05 11:12:50 +0200

    fix: add content to about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -6,8 +6,26 @@ export function About() {
       <h2>Om mig</h2>
       <img src="/images/me.png" alt="" />
       <p>
-        Efter över fyrtio år som formgivande guldsmed, är det nu helt nya
-        tekniker som jag arbetar med.
+        Jag har hela livet jobbat med skapande på något sätt. Jag utbildade mig
+        till guldsmed och har tillsammans med min fru Karin arbetat i fyrtio år
+        i vår egen guldsmedsateljé i Norrtälje.
+      </p>
+      <p>
+        Numera är det stora stenar som får ge uttryck för mina kreativa idéer.
+        Jag har skulpterat i sten sedan 2017. För mig är det en rogivande
+        skaparprocess som långsamt tar form. Det är för det mesta stenen som
+        avgör vad resultatet ska bli, den inspirerar till något. Former som
+        uppstår under arbetets gång kan ändra på allt. Den färdiga skulpturen är
+        många gånger inte lik ursprungstanken.
+      </p>
+      <p>
+        Jag måste känna glädjen i arbetet och friheten att göra precis vad som
+        just i stunden faller mig in. Det kan bli flera skulpturer av samma idé,
+        men av olika stenar, det ena föder det andra.
+      </p>
+      <p>
+        Kreativiteten i mitt huvud står nog aldrig still, jag ser former och
+        linjer överallt. Det är det som är min drivkraft!
       </p>
     </article>
   );
