13343afc79d9bd05e288187d9aea4cd61d280b36
j-carlander <johannes.carlander@gmail.com>
2024-10-25 15:00:55 +0200

    style: header height

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,30 @@
 import { useNavigate } from "react-router-dom";
 import { Nav } from "../Nav/Nav";
 import "./Header.css";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 export function Header() {
-  const [pageScrolled, setPageScrolled] = useState(false);
-
   const navigate = useNavigate();
 
-  useEffect(() => {
-    function handleYOffset() {
-      if (window.matchMedia("(max-width: 700px)").matches) {
-        if (window.scrollY > 180 && pageScrolled === false) {
-          setPageScrolled((scrolled) => !scrolled);
-        } else if (window.scrollY < 100 && pageScrolled === true) {
-          setPageScrolled((scrolled) => !scrolled);
-        }
-      }
-    }
-    window.addEventListener("scroll", handleYOffset);
-    return () => window.removeEventListener("scroll", handleYOffset);
-  }, [pageScrolled]);
+  // const [pageScrolled, setPageScrolled] = useState(false);
+
+  // useEffect(() => {
+  //   function handleYOffset() {
+  //     if (window.matchMedia("(max-width: 700px)").matches) {
+  //       if (window.scrollY > 180 && pageScrolled === false) {
+  //         setPageScrolled((scrolled) => !scrolled);
+  //       } else if (window.scrollY < 100 && pageScrolled === true) {
+  //         setPageScrolled((scrolled) => !scrolled);
+  //       }
+  //     }
+  //   }
+  //   window.addEventListener("scroll", handleYOffset);
+  //   return () => window.removeEventListener("scroll", handleYOffset);
+  // }, [pageScrolled]);
 
   return (
     <header className="page-header">
-      <div
-        className={pageScrolled ? "header-wrapper compact " : "header-wrapper"}>
+      <div className={"header-wrapper"}>
         <h1 className="page-title" onClick={() => navigate("/")}>
           Stenart <span className="page-subtitle">S.C.Flummer</span>
         </h1>
